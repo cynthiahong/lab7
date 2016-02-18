a3d639d5c62963251553749883329cbd7a281adf
vagrant <vagrant@precise32.(none)>
2016-02-18 20:45:32 +0000

    Added Experiment Header

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,19 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+  var random_num = Math.random();
+  console.log(random_num);
+
+  if (random_num >= 0) {
+    projects['grid'] = false;
+    res.render('index', projects);
+  } else {
+    res.redirect('/grid');
+  }
+};
+
+
+exports.view = function(req, res){
     projects["grids"] = false;
   	res.render('index', projects);
 
